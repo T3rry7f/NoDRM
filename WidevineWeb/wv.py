9b035da621ef46bf69a73951336bf5be209a3c9c
T3rry7f <731531775@qq.com>
2021-08-15 19:08:04 +0800

    update

--- WidevineWeb/wv.py
+++ WidevineWeb/wv.py
@@ -84,7 +84,7 @@ class WidevineCDM:
         if(self.pssh_type=='mpd'): # parse mpd file 
             try:
 
-                matchObj = re.findall(r'pssh.+<', data.decode('utf-8'), re.M|re.I)
+                matchObj = re.findall(r'ed.+\n.+(pssh.+)<', data.decode('utf-8'), re.M | re.I)
                 pssh_data= matchObj[0].split('>')[1].split('<')[0]
                 self.pssh_data=base64.b64decode(pssh_data)
             except Exception as e:
